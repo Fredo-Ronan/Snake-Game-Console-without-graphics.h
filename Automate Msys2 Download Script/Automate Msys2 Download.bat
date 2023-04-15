dd84b205997177954f15217baf531e7d8b60c9e4
Fredo Code <128687596+Fredo-Ronan@users.noreply.github.com>
2023-04-15 23:57:52 +0700

    Add files via upload

---
+++ Automate Msys2 Download Script/Automate Msys2 Download.bat
@@ -0,0 +1,6 @@
+@echo off
+powershell -ExecutionPolicy Bypass -File "C:\\File_Download_1.ps1"
+
+Rem {IMPORTANT NOTES} :
+Rem This Batch File is not perfect yet, stay tune for more updated automate GCC download script
+Rem Batch File ini belum sempurna, mohon menunggu update selanjutnya untuk script download otomatis GCC
---
+++ Automate Msys2 Download Script/msys2_download_and_install.ps1
@@ -0,0 +1,12 @@
+$URL="https://github.com/msys2/msys2-installer/releases/download/2023-03-18/msys2-x86_64-20230318.exe"
+$PathDownload=(New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+$NameFile="\msys2-x86_64-20230318.exe"
+$Path=$PathDownload+$NameFile
+
+Start-BitsTransfer -Source $URL -Destination $Path
+
+write-output "Starting the GCC Installer....."
+timeout /t 10 /nobreak
+
+cd "C:\Users\DELL\Downloads\"
+./msys2-x86_64-20230318
